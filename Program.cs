075e556e1c820fc0dcbdd0bb03e93c2adae7a9d4
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-08-09 23:48:30 -0300

    :heavy_check_mark: test(Program.cs): Adicionando execução de testes para implementações do desafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//* IMPLEMANTADO AQUI
+
+
+Console.WriteLine("--------------------------------------\n MEU TESTE: \n--------------------------------------");
+
+Nokia nk1 = new Nokia("(83) 91234-5678", "Nokia NKA 22", "3335", 32);
+Iphone ip1 = new Iphone("(83) 98765-4321", "Iphone 15", "2222", 128);
+
+Console.WriteLine("(Teste Nokia)");
+nk1.Ligar();
+nk1.InstalarAplicativo("Snake game");
+nk1.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("(Teste Iphone)");
+ip1.Ligar();
+ip1.InstalarAplicativo("LinkedIn");
+ip1.ReceberLigacao();
+
+
+
+Console.WriteLine("--------------------------------------\n TEMPLATE TESTE: \n--------------------------------------");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iphone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
