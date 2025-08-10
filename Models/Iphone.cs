5521065898115c28d3b906530e8123279a135475
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-08-09 23:39:28 -0300

    :arrow_up: feat(Iphone): Implementando modelagem do iphone conforme diagrama do desafio DIO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    //* IMPLEMANTADO AQUI
+
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        //* IMPLEMANTADO AQUI
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($@"Instalando aplicativo ""{nomeApp}"" no iphone...");
+        }
     }
 }
