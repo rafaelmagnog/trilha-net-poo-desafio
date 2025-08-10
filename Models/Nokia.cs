12f982db4da74b5c7d599b9004801e8cce66c37d
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-08-09 23:40:46 -0300

    :arrow_up: feat(Nokia): Implementado modelagem do Nokia conforme diagrama do desafio DIO

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    //* IMPLEMANTADO AQUI
+
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        //* IMPLEMANTADO AQUI
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($@"Instalando aplicativo ""{nomeApp}"" no Nokia...");
+        }
     }
 }
